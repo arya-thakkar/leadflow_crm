eeff10d136be8ec3b7f592d6e5713a771a572a42
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-07 01:12:59 +0530

    Fix CORS configuration in server.js

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: *,
-  credentials: true,
+  origin: '*'
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
