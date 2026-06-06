ee38edb4851ec2e3b45737168a37666099d727e5
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-07 01:20:40 +0530

    chore: remove frontend folder, keep only README

--- crm/frontend/src/pages/AnalyticsPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AnalyticsDashboard from '../components/analytics/AnalyticsDashboard';
-
-export default function AnalyticsPage() {
-  return <AnalyticsDashboard />;
-}
