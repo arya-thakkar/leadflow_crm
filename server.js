7f364496dec6d45808d11c897e3cbe76030ac667
arya-thakkar <a.thakkar071006@gmail.com>
2026-06-07 01:09:59 +0530

    Allow all origins for CORS

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: *,
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
